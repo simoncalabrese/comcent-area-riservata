3020cfbb672b598721ebe4f6cba2cb1f2dfb8efb
simon.calabrese <bartolomeomio8>
2017-11-30 12:10:16 +0100

    fix pom

--- comcent-area-riservata-controller/src/main/java/comcent/controller/Main.java
+++ comcent-area-riservata-controller/src/main/java/comcent/controller/Main.java
@@ -8,7 +8,14 @@ import org.springframework.boot.SpringApplication;
  * Created by simon.calabrese on 31/10/2017.
  */
 public class Main extends BaseApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Main.class, args);
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+                System.out.println("Shutdown hook ran!")
+        ));
+
+        while (true) {
+            Thread.sleep(1000);
+        }
     }
 }
